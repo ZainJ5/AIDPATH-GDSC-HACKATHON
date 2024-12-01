2af616033b484cbc3c76e54b2d523e032e5a97e6
Zain Jamshaid <154011623+ZainJ5@users.noreply.github.com>
2024-12-01 06:14:53 +0500

    Update SampleMap.jsx

--- src/components/SampleMap.jsx
+++ src/components/SampleMap.jsx
@@ -5,6 +5,22 @@ import { GiPathDistance } from "react-icons/gi";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
+
 const SampleMap = ({ 
   center, 
   zoom, 
