9e228c701a83d7179ca831884d3de456bbba8e52
Zain Jamshaid <154011623+ZainJ5@users.noreply.github.com>
2024-12-01 11:56:08 +0500

    Update SampleMap.jsx

--- src/components/SampleMap.jsx
+++ src/components/SampleMap.jsx
@@ -4,8 +4,16 @@ import { FaAmbulance, FaHospitalAlt } from "react-icons/fa";
 import { GiPathDistance } from "react-icons/gi";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapPin } from "lucide-react"; 
+import { DefaultIcon } from '../utils/iconConfig';
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  DefaultIcon
+});
 
 const SampleMap = ({ 
   center, 
@@ -32,19 +40,6 @@ const SampleMap = ({
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return R * c;
   };
-
-  const userIcon = new L.divIcon({
-    className: 'custom-div-icon',
-    html: `<div style="color: #2563eb; background-color: white; border-radius: 50%; padding: 2px; box-shadow: 0 2px 4px rgba(0,0,0,0.2);">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-              <circle cx="12" cy="10" r="3"></circle>
-            </svg>
-           </div>`,
-    iconSize: [30, 30],
-    iconAnchor: [15, 30],
-    popupAnchor: [0, -30]
-  });
 
   const calculateTime = (distance) => {
     const averageSpeed = 40;
@@ -105,7 +100,7 @@ const SampleMap = ({
 
   const fetchRouteToHospital = async (lat, lon, hospital) => {
     const routeResponse = await fetch(
-      `http://router.project-osrm.org/route/v1/driving/${lon},${lat};${hospital.lon},${hospital.lat}?overview=full&geometries=polyline
+      `https://router.project-osrm.org/route/v1/driving/${lon},${lat};${hospital.lon},${hospital.lat}?overview=full&geometries=polyline
     `);
     const routeData = await routeResponse.json();
 
@@ -193,8 +188,8 @@ const SampleMap = ({
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
           />
-          <Marker position={userLocation} icon={userIcon}>
-          <Popup>You are here</Popup>
+          <Marker position={userLocation}>
+            <Popup>You are here</Popup>
           </Marker>
           {showHospitals &&
             hospitals.map((hospital, index) => {
